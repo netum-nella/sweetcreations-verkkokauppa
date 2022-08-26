58f6efae10981d372b9e80d984b3c3026aa09957
LindaOljemark <linda.oljemark@awacademy.training>
2022-08-26 09:06:54 +0300

    dev-branch first commit, template update

---
+++ SweetCreations-main/Sweet/src/handlers/createtablelambda.js
@@ -0,0 +1,24 @@
+//create lambda function to create table in Aurora serverless database environment postgresql CREATE TABLE customer( customer_id INT GENERATED ALWAYS AS IDENTITY, customer_name VARCHAR(255) NOT NULL, customer_city varchar(255), customer_phone integer, customer_email varchar(255) NOT NULL, PRIMARY KEY(customer_id));
+const pg = require("pg");
+const AWS = require("aws-sdk");
+
+exports.createtablelambdaHandler = async (event, context) => {
+  const db = pg.createConnection({
+    host: "",
+    port: 1234,
+    user: "",
+    password: "",
+    database: "",
+  });
+  await db.connect();
+  const statement =
+    "CREATE TABLE customer (customer_id INT GENERATED ALWAYS AS IDENTITY, customer_name VARCHAR(255) NOT NULL, customer_city varchar(255), customer_phone integer, customer_email varchar(255) NOT NULL, PRIMARY KEY(customer_id));";
+  await db.query(statement);
+  await db.end();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Table created successfully",
+    }),
+  };
+};
