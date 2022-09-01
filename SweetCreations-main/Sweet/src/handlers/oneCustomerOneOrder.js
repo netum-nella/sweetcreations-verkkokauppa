d1c4c13df4ea5a7d2113d9269bc0d3a70a0d8021
nellarosaliina <nella.heikkila@windowslive.com>
2022-09-01 16:21:31 +0300

    koko projekti paketissa

--- SweetCreations-main/Sweet/src/handlers/oneCustomerOneOrder.js
+++ SweetCreations-main/Sweet/src/handlers/oneCustomerOneOrder.js
@@ -15,7 +15,15 @@ exports.oneCustomerOneOrderHandler = async (event, context) => {
 
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin":
+        "http://sweety-bucket.s3-website.us-east-2.amazonaws.com", // replace with hostname of frontend (CloudFront)
+      "Access-Control-Allow-Headers":
+        "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Methods, Acces-Control-Allow-Origin",
+      "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
+    },
     body: JSON.stringify(res.rows),
   };
+
   return response;
 };
--- SweetCreations-main/Sweet/src/handlers/oneCustomerShoppingcart.js
+++ SweetCreations-main/Sweet/src/handlers/oneCustomerShoppingcart.js
@@ -15,6 +15,13 @@ exports.oneCustomerShoppingcartHandler = async (event, context) => {
 
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin":
+        "http://sweety-bucket.s3-website.us-east-2.amazonaws.com", // replace with hostname of frontend (CloudFront)
+      "Access-Control-Allow-Headers":
+        "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Methods, Acces-Control-Allow-Origin",
+      "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
+    },
     body: JSON.stringify(res.rows),
   };
 
